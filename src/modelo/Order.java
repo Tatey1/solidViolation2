b93dd0d3f45b16a836b1d02b2dddc0c1a789e1f4
Tatey1 <123716292+Tatey1@users.noreply.github.com>
2024-11-14 13:23:51 -0500

    Update Order.java

--- src/modelo/Order.java
+++ src/modelo/Order.java
@@ -1,15 +1,14 @@
 package modelo;
 
-import bd.DatabaseMysql;
-
 public class Order {
-
     private String orderId;
     private double amount;
+    private Database database;
 
-    public Order(String orderId, double amount) {
+    public Order(String orderId, double amount, Database database) {
         this.orderId = orderId;
         this.amount = amount;
+        this.database = database;
     }
 
     public String getOrderId() {
@@ -21,11 +20,8 @@ public class Order {
     }
 
     public void saveOrder() {
-        // Lógica para guardar la orden en la base de datos
-        //Envio a guardar en base de datos
-        DatabaseMysql databaseMysql = new DatabaseMysql();
         System.out.println("Order saved in database");
-        databaseMysql.guardar();
+        database.guardar();
     }
 
     public String generateOrderReport() {
