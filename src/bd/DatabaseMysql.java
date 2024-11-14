5ad9e50d0185050c026b42ca451859a2cebbc090
Tatey1 <123716292+Tatey1@users.noreply.github.com>
2024-11-14 13:25:27 -0500

    Update DatabaseMysql.java

--- src/bd/DatabaseMysql.java
+++ src/bd/DatabaseMysql.java
@@ -1,10 +1,11 @@
 package bd;
 
-public class DatabaseMysql {
+import modelo.Database;
+
+public class DatabaseMysql implements Database{
 
     public void guardar() {
         System.out.println("Creo conexion a la base de datos");
         System.out.println("Guarde en la base de datos");
-        //Abril estuvo aca otra vez :)
     }
 }
