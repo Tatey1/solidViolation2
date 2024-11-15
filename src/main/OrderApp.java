d335b0287cfa5fa94707725ab689c1e4c232f0ac
Tatey1 <123716292+Tatey1@users.noreply.github.com>
2024-11-15 16:07:22 -0500

    Update OrderApp.java

--- src/main/OrderApp.java
+++ src/main/OrderApp.java
@@ -1,17 +1,17 @@
 package main;
 
 import modelo.Order;
-import modelo.Database;
+import modelo.OrdersDatabase;
 import bd.DatabaseMysql;
 
 public class OrderApp {
     public static void main(String[] args) {
         // Crear una orden con un ID y un monto
-        Database orderDatabase = new DatabaseMysql();
-        Order order = new Order("ORD123", 250.75, orderDatabase);
+        Order order = new Order("ORD123", 250.75);
 
         // Guardar la orden en la base de datos
-        order.saveOrder();
+        OrdersDatabase ordersDatabase = new OrdersDatabase(new DatabaseMysql());
+        ordersDatabase.saveOrder();
 
         // Generar y mostrar el reporte de la orden
         String report = order.generateOrderReport();
